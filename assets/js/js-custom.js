ac0e303970d85920940099d7144a69573cdaf45a
Phatnu <cuetop175@gmail.com>
2024-11-28 20:48:20 +0800

    First commit
    First Date: 28/11/24

---
+++ assets/js/js-custom.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-input", {
+
+    strings: ["-to create a Web application with responsive design and multiple function using C# language."],
+    typeSpeed: 50,
+    loop: false
+
+})
